e25bf18cb0ca97f7f3d2f0a2eb792bb2c6067ad6
ZavbakGit <ag@799000.ru>
2020-08-03 18:26:40 +1000

    111

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -7,10 +7,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class UserBloc extends Bloc<UserEvent,UserState>{
   final UsersRepository usersRepository;
 
-  UserBloc({this.usersRepository}):assert(usersRepository != null);
+  UserBloc({this.usersRepository}) : super(UserEmptyState());
 
-  @override
-  UserState get initialState => UserEmptyState();
+//  UserBloc({this.usersRepository}):assert(usersRepository != null);
+
+//  @override
+//  UserState get initialState => UserEmptyState();
 
   @override
   Stream<UserState> mapEventToState(UserEvent event) async* {
@@ -26,4 +28,6 @@ class UserBloc extends Bloc<UserEvent,UserState>{
       yield UserEmptyState();
     }
   }
+
+
 }
